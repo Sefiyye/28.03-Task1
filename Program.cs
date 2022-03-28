d4b450cb219a89df7ba526f1eb1919390e3a8b31
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-28 09:45:24 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _28._03_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Salam     Dunya";
+            Console.WriteLine(whiteSpaceX(word));
+        }
+        static string whiteSpaceX(string sentence)
+        {
+            int count = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == ' ')
+                {
+                    count++;
+                    if (count > 1)
+                    {
+                        int firstIndex = sentence.IndexOf(' ');
+                        int lastIndex = sentence.LastIndexOf(' ');
+                        int difference = lastIndex - firstIndex;
+                        sentence = sentence.Remove(firstIndex, difference);
+                    }
+                }
+            }
+            return sentence;
+        }
+    }
+}
